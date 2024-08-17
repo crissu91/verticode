8a601d25e961fce46fc13069ed4c8ba0252e2b15
crissu91 <cristinaf.stefan@gmail.com>
2024-08-17 10:47:30 +0100

    Changes to styling and adds image

--- src/components/SingleProject.tsx
+++ src/components/SingleProject.tsx
@@ -6,15 +6,24 @@ function SingleProject() {
     const formattedDate = new Date(newProject.startDate).toLocaleString();
 
     return (
-        <div className='flex flex-col mx-auto space-y-4 py-10 w-[23em] border border-green rounded-[12px] shadow-md mt-10 px-8'>
+    <div className='flex gap-10 mx-auto border border-green rounded-[12px] shadow-md py-10 px-8 mt-10'>
+        <img src={newProject.imageUrl} alt="Project image" className='object-cover '/>
+            
+        <div className='flex flex-col space-y-4 w-[23em]'>
             <h1 className='text-[24px] font-medium'>Project name: {newProject.projectName}</h1>
 
             <p>Category: {newProject.category}</p>
             
             <p>Start date: { formattedDate}</p>
 
-            <p>Description: { newProject.description}</p>
+            <p>Description: {newProject.description}</p>
+            
+            <div className='flex gap-4'>
+                <label htmlFor='completed'>Mark as completed</label>
+                <input type="checkbox" id="completed" name="completed" value="completed"></input>
+            </div>
         </div>
+    </div>
     )
 }
 export default SingleProject;
