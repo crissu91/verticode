b9c50c2531a29bdb81ec8bb9dfffe18e1f35a9b5
crissu91 <cristinaf.stefan@gmail.com>
2024-08-17 10:06:14 +0100

    Move project form into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,6 @@ import { useState } from "react";
 import { IProject } from "./models/Project";
 
 export default function App() {
-  const [newProject, setNewProject] = useState<IProject>({
-    projectName: "",
-    description: "",
-    startDate: new Date(),
-    category: "",
-  });
-  const formHasErrors =  Object.values(newProject).some((items: keyof IProject) => items.length ===0) 
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    setNewProject((prevState) => ({
-      ...prevState,
-      projectName: newProject.projectName,
-      description: newProject.description,
-      startDate: newProject.startDate,
-      category: newProject.category,
-    }));
-  };
-
   return (
     <main className="flex flex-col">
       <header className="flex h-24 bg-light-green">
